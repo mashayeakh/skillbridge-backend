16cc5f57eee7a0325794ad72522e4bcb427860f3
mashayeakh <islammasayekh@gmail.com>
2026-05-05 03:32:56 +0600

    Fix Prisma adapter initialization and Vercel compatibility

--- src/module/student/student.controller.ts
+++ src/module/student/student.controller.ts
@@ -1,7 +1,6 @@
 import { asyncHandler } from './../../utils/asyncHandler';
 import { Request, Response } from "express";
 import { StudentService } from './student.service';
-import { success } from 'better-auth/*';
 import { BookingService } from '../bookings/bookings.service';
 import { AppError } from '../../error/appErrors';
 import { auth } from '../../lib/auth';
